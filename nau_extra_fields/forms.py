bcd4ad9cb4a6d22ac3ca0a39a8f0eb3dcf1ec3cf
Bryann Valderrama <bryann.valderrama@edunext.co>
2025-07-31 08:45:04 -0500

    feat: add custom model and form

---
+++ nau_extra_fields/forms.py
@@ -0,0 +1,121 @@
+"""
+Forms for nau_extra_fields.
+
+According to edx-platform FormDescription:
+https://github.com/fccn/edx-platform/blob/315ca44d10d47bf4e16f0c916295c70939cf27e1/openedx/core/djangoapps/user_api/helpers.py#L95
+
+ALLOWED_TYPES = [
+    "text", "email", "select", "textarea", "checkbox", "plaintext", "password", "hidden"
+]
+
+ALLOWED_RESTRICTIONS = {
+    "text": ["min_length", "max_length"],
+    "password": [
+        "min_length",
+        "max_length",
+        "min_upper",
+        "min_lower",
+        "min_punctuation",
+        "min_symbol",
+        "min_numeric",
+        "min_alphabetic",
+    ],
+    "email": ["min_length", "max_length", "readonly"],
+}
+
+FIELD_TYPE_MAP = {
+    forms.CharField: "text",
+    forms.PasswordInput: "password",
+    forms.ChoiceField: "select",
+    forms.TypedChoiceField: "select",
+    forms.Textarea: "textarea",
+    forms.BooleanField: "checkbox",
+    forms.EmailField: "email",
+}
+"""
+
+from django import forms
+from django.forms import ModelForm
+
+from nau_extra_fields.models import NauUserExtendedModel
+
+
+class ExtraInfoForm(ModelForm):
+    """
+    Form that represents user extra info and is compatible with edX's FormDescription system.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Text field with restrictions
+        self.fields["full_name"].help_text = "Enter your full name."
+        self.fields["full_name"].min_length = 3
+        self.fields["full_name"].max_length = 100
+        self.fields["full_name"].required = True
+        self.fields["full_name"].restrictions = {
+            "min_length": 3,
+            "max_length": 100,
+        }
+
+        # Textarea field
+        self.fields["bio"].widget = forms.Textarea()
+        self.fields["bio"].help_text = "Write a short bio."
+        self.fields["bio"].field_type = "textarea"
+        self.fields["bio"].required = True
+        self.fields["bio"].error_messages = {
+            "required": "Please tell us something about yourself",
+        }
+
+        # Checkbox
+        self.fields["wants_newsletter"].label = "Subscribe to newsletter?"
+
+        # Select field
+        self.fields["favorite_language"].help_text = "Pick your preferred programming language."
+
+        # Plaintext hint
+        self.fields["password_hint"].help_text = "This will help you remember your password."
+        self.fields["password_hint"].placeholder = "Insert a hint"
+        self.fields["password_hint"].min_length = 5
+        self.fields["password_hint"].max_length = 10
+        self.fields["password_hint"].required = True
+        self.fields["password_hint"].error_messages = {
+            "required": "Insert a hint for your password",
+            "min_length": "Hint is too short",
+            "max_length": "Hint is too long",
+        }
+
+        # Checkbox with custom message
+        self.fields["data_authorization"].error_messages = {
+            "required": "Please authorize data processing.",
+        }
+
+        # Agreement checkbox
+        self.fields["terms_accepted"].label = "I accept the Terms and Conditions"
+        self.fields["terms_accepted"].required = True
+
+        self.fields["favorite_language"].required = True
+
+    class Meta:
+        model = NauUserExtendedModel
+        fields = [
+            "data_authorization",
+            "full_name",
+            "bio",
+            "wants_newsletter",
+            "favorite_language",
+            "password_hint",
+            "terms_accepted",
+        ]
+
+        # According to edx-platform RegistrationFormFactory
+        #
+        # https://github.com/fccn/edx-platform/blob/315ca44d10d47bf4e16f0c916295c70939cf27e1/openedx/core/djangoapps/user_authn/views/registration_form.py#L457-L458
+        #
+        # here you have to use the same names as in the model and you can add default,
+        # field_type and include_default_option
+        serialization_options = {
+            "password_hint": {
+                "default": "Funny Password",
+            },
+        }
--- nau_extra_fields/models.py
+++ nau_extra_fields/models.py
@@ -2,11 +2,15 @@
 Database models for nau_extra_fields.
 """
 
-# from django.db import models
-from model_utils.models import TimeStampedModel
+from django.contrib.auth import get_user_model
+from django.core.validators import MinLengthValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
 
 
-class NauUserExtendedModel(TimeStampedModel):
+class NauUserExtendedModel(models.Model):
     """
     TODO: replace with a brief description of the model.
 
@@ -15,11 +19,70 @@ class NauUserExtendedModel(TimeStampedModel):
     https://open-edx-proposals.readthedocs.io/en/latest/oep-0030-arch-pii-markup-and-auditing.html
     """
 
-    # TODO: add field definitions
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+
+    data_authorization = models.BooleanField(
+        verbose_name=_(
+            "I have read and understood the <a href='https://www.nau.edu.pt/legal/politica-de-privacidade/'"
+            " rel='noopener' target='_blank'>Privacy Policy</a>"
+        )
+    )
+
+    full_name = models.CharField(
+        blank=True,
+        max_length=255,
+        validators=[MinLengthValidator(2)],
+        verbose_name=_("Full name"),
+    )
+
+    bio = models.TextField(
+        blank=True,
+        verbose_name=_("Biography"),
+        help_text=_("Tell us a bit about yourself."),
+    )
+
+    wants_newsletter = models.BooleanField(
+        default=False,
+        verbose_name=_("Subscribe to newsletter"),
+    )
+
+    favorite_language = models.CharField(
+        blank=True,
+        max_length=50,
+        choices=[
+            ("python", "Python"),
+            ("javascript", "JavaScript"),
+            ("java", "Java"),
+            ("go", "Go"),
+        ],
+        verbose_name=_("Favorite programming language"),
+    )
+
+    password_hint = models.CharField(
+        max_length=255,
+        blank=True,
+        verbose_name=_("Password hint"),
+        help_text=_("Optional hint to remember your password."),
+    )
+
+    terms_accepted = models.BooleanField(
+        default=False,
+        verbose_name=_("I accept the Terms and Conditions."),
+    )
+
+    registration_token = models.CharField(
+        blank=True,
+        max_length=64,
+        editable=False,
+        verbose_name=_("Registration token (internal)"),
+    )
+
+    class Meta:
+        verbose_name = _("Extra Info")
+        verbose_name_plural = _("Extra Info entries")
 
     def __str__(self):
         """
         Get a string representation of this model instance.
         """
-        # TODO: return a string appropriate for the data fields
-        return "<NauUserExtendedModel, ID: {}>".format(self.id)
+        return f"<NauUserExtendedModel, ID: {self.id}, User: {self.user.username}>"
